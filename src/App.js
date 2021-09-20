9d2b991931f4996019ae9acc25646f03750453ad
gerorajoy21 <gerorajoy21@gmail.com>
2021-09-19 23:11:40 -0300

    subiendo archivos

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tienda de Videojuegos</h1>
+      <p>Somos la mejor tienda online de Videojuegos de Argentina</p>
+    </div>
+  );
+}
+
+export default App;
