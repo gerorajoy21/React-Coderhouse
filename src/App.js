b8621f713e2dab813f07edc0fc2b73553b91d131
gerorajoy21 <84244049+gerorajoy21@users.noreply.github.com>
2021-09-29 23:15:44 -0300

    Desafio clase 4

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import React, { Component } from "react";
+import React from 'react';
+import './App.css';
 
-// Components
-import NavBar from "./components/NavBar";
-import "./components/NavBar.css";
+import NavBar from './components/NavBar';
+import ItemListContainer from './components/ItemListContainer';
 
-let city = "Buenos Aires";
+class App extends React.Component {
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <NavBar />
-        <h1 style={{ textAlign: "center"}}>
-          Tienda de Videojuegos
-        </h1>
-        <p style={{ textAlign: "center"}}>La mejor plataforma de videojeugos de {city}</p>
-      </div>
-    );
-  }
+	render() {
+		const titulo = 'GTA 5';
+		return (
+			<div className='App'>
+        <div>
+          <NavBar />
+        </div>
+				<div className='UserSection d-flex justify-content-evenly mt-5'>
+					<ItemListContainer
+						juego={titulo}
+						precio='$ 5000'
+						description='El mundo abierto mas vendido de la historia. En Gta 5 vas a poder hacer lo que no te animas a hacer en la vida real'
+						img='https://react.semantic-ui.com/images/avatar/large/matthew.png'
+					/>
+					<ItemListContainer
+						juego='Fifa 22'
+						precio='$11000'
+						description='El nuevo videojuego de futbol de Ea Sports salio a la venta! No te pierdas la oportunidad de jugar con tu equipo favorito.'
+						img='https://react.semantic-ui.com/images/avatar/large/elliot.jpg'
+					/>
+					<ItemListContainer
+						juego='Fortnite'
+						precio='GRATIS'
+						description='Juega el battle royale mas famoso del momento totalmente gratis y con tus amigos!'
+						img='https://react.semantic-ui.com/images/avatar/large/daniel.jpg'
+					/>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
