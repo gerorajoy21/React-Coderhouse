ca2e16b6aa08a75a9555b8f125661b4ac1d5c47d
gerorajoy21 <84244049+gerorajoy21@users.noreply.github.com>
2021-09-29 23:14:08 -0300

    Add files via upload

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,51 @@
-const NavBar = () => {
+import React from 'react';
+
+//Components
+import "./CartWidget";
+import CartWidget from './CartWidget';
+import { Icon } from "semantic-ui-react";
+
+
+function NavBar() {
     return (
-      <nav>
-          <h3>Gaming Varela</h3>
-        <ul>
-          <li><a href="#">Inicio</a></li>
-          <li><a href="#">Nosotros</a></li>
-          <li><a href="#">Tienda</a></li>
-          <li><a href="#">Contacto</a></li>
-        </ul>
-      </nav>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+  <div className="container-fluid">
+  <a className="navbar-brand" href="#">
+  <Icon disabled name="gamepad" />
+      Varela Play
+    </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Catalogo</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Nosotros</a>
+        </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Dropdown link
+          </a>
+          <ul className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a className="dropdown-item" href="#">Action</a></li>
+            <li><a className="dropdown-item" href="#">Another action</a></li>
+            <li><a className="dropdown-item" href="#">Something else here</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+    <div>
+        <CartWidget/>
+      </div>
+  </div>
+
+</nav>
     );
   };
   
