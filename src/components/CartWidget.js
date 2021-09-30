2fe683891c892ebda124f666a41f895bfdc0fcdd
gerorajoy21 <84244049+gerorajoy21@users.noreply.github.com>
2021-09-29 23:14:57 -0300

    Desafio clase 4

---
+++ src/components/CartWidget.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Icon } from "semantic-ui-react";
+
+const CartWidget = () => {
+    return (
+        <Icon className="bg-light" disabled name="cart" />
+    );
+  };
+  
+  export default CartWidget;
